42971a8a271480340c77e71d6082e1e2da920648
sanazez <sanazez1234@gmail.com>
2020-09-15 20:29:41 +0300

    feat: added solutin dream-team

--- src/dream-team.js
+++ src/dream-team.js
@@ -1,6 +1,14 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function createDreamTeam(/* members */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
-};
+module.exports = function createDreamTeam(arrayNames) {
+    let arrFirstLetter = [];
+    if (Array.isArray(arrayNames)) {
+        for (let i = 0; i < arrayNames.length; i++) {
+            if (arrayNames[i] && typeof arrayNames[i] == 'string') {
+                let lettter = arrayNames[i].trim()[0].toUpperCase();
+                arrFirstLetter.push(lettter);
+            }
+        }
+        return arrFirstLetter.sort().join('');
+    } else return false;
+}

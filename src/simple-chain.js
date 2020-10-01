09c0ca453368b915821f26c4097558cba1689cd4
sanazez <sanazez1234@gmail.com>
2020-10-01 19:54:25 +0300

    fix: added solution hanoi-tower

--- src/simple-chain.js
+++ src/simple-chain.js
@@ -1,26 +1,47 @@
 const CustomError = require("../extensions/custom-error");
 
 const chainMaker = {
-  getLength() {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
-  },
-  addLink(value) {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
-  },
-  removeLink(position) {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
-  },
-  reverseChain() {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
-  },
-  finishChain() {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
-  }
+    chain: [],
+    getLength() {
+        return this.chain.length;
+    },
+    addLink(value) {
+        this.chain.push(value);
+        return this;
+    },
+    removeLink(position) {
+        if (typeof(position) !== 'number') {
+            this.chain = [];
+            throw new Error();
+        }
+
+        position--;
+        if (position < 0 || position >= this.getLength()) {
+            this.chain = [];
+            throw new Error();
+        }
+
+        this.chain.splice(position, 1);
+        return this;
+    },
+    reverseChain() {
+        this.chain.reverse();
+        return this;
+    },
+    finishChain() {
+        let str = '';
+
+        for (let i = 0; i < this.getLength(); ++i) {
+            str += `( ${this.chain[i]} )`;
+
+            if (i < this.getLength() - 1) {
+                str += '~~';
+            }
+        }
+
+        this.chain = [];
+        return str;
+    }
 };
 
 module.exports = chainMaker;
